3c1ca6a8d720ebd7b471982910c715e5b9eef449
ric-e <136038531+ric-e@users.noreply.github.com>
2025-02-19 17:40:47 -0500

    Attempt 3

--- P2.py
+++ P2.py
@@ -8,48 +8,34 @@ cipher = list(input())
 c = int(input())
 
 cipher_length = 0
-cipher_list = []
-cipher_value = []
+cipher_list = []    # stores repeat counts
+cipher_value = []   # stores characters
 saved_value = ""
+
 for i in range(len(cipher)):
-    if i == 0:
-        cipher_value.append(cipher[i])
-    elif cipher[i].isdigit() == False:
-        # if it is not a number
-        # check to see if the previous value was a number
-        if cipher[i-1].isdigit() == True:
-            # step 1: save the current character as a cipher value
-            
-            cipher_value.append(cipher[i])
-            # step 2: save the previous number to the cipher length
-            cipher_list.append(int(saved_value))
+    if cipher[i].isalpha():
+        cipher_list.append(cipher[i])
+        if cipher[i-1].isnumeric() and i != 0:
+            cipher_value.append(int(saved_value))
             cipher_length += int(saved_value)
-        else:
-            cipher_value.append(cipher[i])
-            continue
+            saved_value = ""
     else:
-        # if it is a number
-        if cipher[i-1].isdigit() == True:
-            saved_value += cipher[i]
-        else:
-            # if the previous value was NOT a number, but this one is
-            # we add it to a string called saved_value
-            saved_value = cipher[i]
+        saved_value += cipher[i]
 
-# we need to add the last value to the cipher list
-cipher_list.append(int(saved_value))
+cipher_value.append(int(saved_value))
 cipher_length += int(saved_value)
+# print(cipher_value)
 
-# we now have cipher length and whatever the cipher is
-
-# c is 'cth' character if we repeat infinitely the cipher
-# print(f"the different values are: {cipher_list}")
-# print(f"The characters are {cipher_value}")
-# print(cipher_length)
+rle_list = []
 index = c % cipher_length
-# we now have a value that we're looking for 
-for j in range(len(cipher_list)):
-    if index <= cipher_list[j]:  # If index falls within current character's range
-        print(cipher_value[j])
-        break
-    index -= cipher_list[j]  # Move to next character's range
+# print(cipher_length)
+
+counter = 0 
+for i in cipher_value:
+    for j in range(i):
+        rle_list.append(cipher_list[counter])
+    counter += 1
+
+# rle_list is a numerical list of the cipher (non-repeated tho)
+
+print(rle_list[index])
